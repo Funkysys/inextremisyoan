1e979467757e7a724ca6c9ba94d394aabbdbdc09
Antoine Delbos <aadelbos@gmail.com>
2022-03-22 11:12:50 +0100

    added fixtures

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/{id}', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository, int $id): Response
     {
+        $article = $articleRepository->findAll();
+
+
         return $this->render('home/index.html.twig', [
+            'articles' => $article
         ]);
     }
 }
