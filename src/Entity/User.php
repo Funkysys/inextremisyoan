5c199529e5b26995c49edc75067dba045818e927
Antoine Delbos <aadelbos@gmail.com>
2022-03-28 18:49:04 +0200

    doctrine.yaml update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,7 +36,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private string $passPhrasing ;
 
     public function getId(): ?int
