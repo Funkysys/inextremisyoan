dfe5de6f1d4b4f32a8824bb11ac85a8d245989b5
Antoine Delbos <aadelbos@gmail.com>
2022-03-28 18:41:46 +0200

    doctrine.yaml update

--- .env.local.php
+++ .env.local.php
@@ -6,5 +6,5 @@
 //  'APP_ENV' => 'prod',
 //  'APP_SECRET' => '20abf132d10ff3779a83b1c5dca28571',
 //  'MESSENGER_TRANSPORT_DSN' => 'doctrine://default?auto_setup=0',
-//  'DATABASE_URL' => 'mysql://u737804456_aadelbos:Flora1608@127.0.0.1:3306/u737804456_main?sslmode=disable&charset=utf8mb4',
+//  'DATABASE_URL' => 'mysql://root:@127.0.0.1:3306/main?sslmode=disable&charset=utf8mb4',
 //);
